86c0f1aad3e1669b5d53990251260d8857775476
Nastassia Kryshtal <55696535+NastassiaKryshtal@users.noreply.github.com>
2026-04-16 13:54:50 +0300

    fix: do not pass selectedHighlightId if showOccurrences is false (#18)
    Co-authored-by: Nataliia Tolokontseva <nataliia_tolokontseva@epam.com>

--- src/components/DocumentPreview/DocumentPreview.tsx
+++ src/components/DocumentPreview/DocumentPreview.tsx
@@ -128,7 +128,7 @@ export const DocumentPreview: FC<DocumentPreviewProps> = ({
     >
       {fileName && (
         <DialEllipsisTooltip
-          className="dial-tiny-text text-secondary"
+          className="dial-tiny-text text-secondary !flex-none"
           text={fileName}
         />
       )}
@@ -218,7 +218,9 @@ export const DocumentPreview: FC<DocumentPreviewProps> = ({
           <PDFViewer
             pdf={file}
             highlights={highlights}
-            selectedHighlightId={highlights[activeHighlightIndex]?.id}
+            selectedHighlightId={
+              showOccurrences ? highlights[activeHighlightIndex]?.id : undefined
+            }
             selectedPageNumber={selectedPageNumber}
             zoom={zoom}
             onTotalPagesChange={onTotalPagesChange}
