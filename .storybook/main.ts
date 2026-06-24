23ec788d8425f4129858e4fa8634837f579e27d3
Nataliia Tolokontseva <129855502+hhharmEpm@users.noreply.github.com>
2026-06-24 20:01:18 +0300

    chore: Upgrade devendencies versions (#44)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,14 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+  viteFinal: async (config) => {
+    config.optimizeDeps ??= {};
+    config.optimizeDeps.exclude = [
+      ...(config.optimizeDeps.exclude ?? []),
+      '@tabler/icons-react',
+    ];
+    return config;
+  },
 };
 
 export default config;
